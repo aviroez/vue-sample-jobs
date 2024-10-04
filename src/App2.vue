9908807fc86fcdd6f640cc62f9f812916f48e6fa
aviroez <avi.roez@gmail.com>
2024-10-04 16:41:48 +0700

    Initial commit

---
+++ src/App2.vue
@@ -0,0 +1,71 @@
+<script setup>
+  import { onMounted, ref } from 'vue';
+
+  const name = ref('John Doe');
+  const status = ref('active');
+  const tasks = ref(['Task One', 'Task Two', 'Task Three']);
+  const newTask = ref('Task Random');
+
+  const toggleStatus = () => {
+    if (status.value == 'active') {
+      status.value = 'pending'
+    } else if (status.value == 'pending') {
+      status.value = 'inactive'
+    } else if (status.value == 'inactive') {
+      status.value = 'active'
+    }
+  }
+
+const addTask = () => {
+  if (newTask.value.trim() !== '') {
+    tasks.value.push(newTask.value);
+    newTask.value = '';
+  }
+}
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1);
+}
+
+onMounted(async() => {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+    const data = await response.json()
+    tasks.value = data.map((task) => task.title)
+  } catch (error) {
+    console.log(error.message)
+  }
+
+})
+</script>
+
+<template>
+  <h1>Vue Jobs: {{ name }}</h1>
+  <p v-if="status == 'active'">User is active</p>
+  <p v-else-if="status == 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask">
+  </form>
+
+  <h3>
+    Task:
+  </h3>
+  <ul>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span>{{ task }}</span>
+      <button @click="deleteTask(index)">x</button>
+    </li>
+  </ul>
+  <a :href="link">Google</a>
+  <p><button v-on:click="toggleStatus">Change Status</button></p>
+  
+</template>
+
+<style scoped>
+  h1{
+    color: aquamarine;
+  }
+</style>
---
+++ src/views/JobsView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Hero from '@/components/Hero.vue'
+import HomeCards from '@/components/HomeCards.vue'
+import JobListings from '@/components/JobListings.vue'
+</script>
+
+<template>
+    <JobListings :showButton="true" />
+</template>
